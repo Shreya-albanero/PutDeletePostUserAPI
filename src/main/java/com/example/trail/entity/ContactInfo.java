36807501a8a6f45b406a9491da0ba3f52a8e5ea1
shreya-albanero <shreya.neema@albanero.io>
2023-01-17 04:41:47 +0530

    Connecting two entity class

---
+++ src/main/java/com/example/trail/entity/ContactInfo.java
@@ -0,0 +1,20 @@
+package com.example.trail.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "contactDb")
+public class ContactInfo {
+    String email;
+    String address;
+    String ph;
+    //int Uid;
+
+}
